ce562b8e6ab53b1cbddd0246ae180d6102c37c8b
Jaakko <54206348+JAAKKQ@users.noreply.github.com>
2022-03-20 14:19:54 +0200

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -3,15 +3,18 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const { dirname } = require('path');
 const RootFolder = dirname(require.main.filename);
 const { token } = require(RootFolder + '/config.json');
+var NumMembers;
 
 client.login(token);
 
 client.once('ready', c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
-    console.log('\x1b[42m%s\x1b[0m', '---------------SERVERS---------------');
+    console.log('\x1b[42m%s\x1b[0m', `---------------IN ${client.guilds.cache.size} SERVERS---------------`);
     client.guilds.cache.forEach(guild => {
         console.log('\x1b[32m%s\x1b[0m', `---------------| ${guild.name} |---------------`);
         console.log(`SERVER ID: ${guild.id}`);
         console.log(`MEMBERS: ${guild.memberCount}`);
+        NumMembers = +[NumMembers] + +[guild.memberCount];
+        console.log('\x1b[34m%s\x1b[0m', `SERVING ${NumMembers} MEMBERS`)
     })
 });
