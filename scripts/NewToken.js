de22fbfd72795c3543c7f6e8acc564eaff4719cb
Jaakko <54206348+JAAKKQ@users.noreply.github.com>
2022-04-09 16:12:33 +0300

    Update NewToken.js

--- scripts/NewToken.js
+++ scripts/NewToken.js
@@ -41,7 +41,7 @@ module.exports = function (dir) {
       const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
-      });      
+      });
       store.load('config', 'token', function (err, object) {
         Token = object
         rl.question('Insert new token: ', function (NewToken) {
@@ -52,126 +52,126 @@ module.exports = function (dir) {
             ClientID = NewID
             if (ClientID.length === 18) {
               console.log('Client ID is valid!');
-              fs.readdir(RootFolder + '/data/member/', function (err, files) {
-                if (err) {
-                  console.error("Could not list the directory.", err);
-                  process.exit(1);
-                }
-                if (files.length) {
-                  var encryptor = require('simple-encryptor')(Token);
-                  var newencryptor = require('simple-encryptor')(newkey);
-                  files.forEach(function (file, index) {
-                    fs.readFile(RootFolder + `/data/member/${file}`, 'utf8', function (err, code) {
-                      if (err) return cb("error loading file" + err);
-                      try {
-                        console.log('\x1b[32m%s\x1b[0m', `-------------------${file}-------------------`);
-                        var jsonObj = JSON.parse(code);
-                        console.log('Decrypting file...');
-                        jsonObj = encryptor.decrypt(jsonObj);
-                        console.log('Output:' + jsonObj);
-                        console.log('Encrypting file with new key...');
-                        jsonObj = newencryptor.encrypt(jsonObj);
-                        console.log('Encrypted file...');
-                        console.log('Output:' + jsonObj);
-                        jsonObj = newencryptor.decrypt(jsonObj);
-                        console.log('Decrypted file with new key...');
-                        console.log('Output:' + jsonObj);
-                        const IsDataGood = IsGoodString(JSON.stringify(jsonObj));
-                        console.log('Is Data Good: ' + IsDataGood);
-                        if (IsDataGood) {
-                          console.log('\x1b[32m%s\x1b[0m', `-------------------------------------------------------------`);
-                          if (files.length === index + 1) {
-                            if (AreFilesGood) {
-                              console.log('Finnished converting all files succesfully!');
-                              console.log('Wrting all files with converted data...');
-                              files.forEach(function (file, index) {
-                                fs.readFile(RootFolder + `/data/member/${file}`, 'utf8', function (err, code) {
-                                  if (err) return cb("error loading file" + err);
-                                  try {
-                                    console.log('\x1b[32m%s\x1b[0m', `-------------------Writing ${file}-------------------`);
-                                    var jsonObj = JSON.parse(code);
-                                    console.log('Decrypting file...');
-                                    jsonObj = encryptor.decrypt(jsonObj);
-                                    console.log('Output:' + jsonObj);
-                                    console.log('Encrypting file with new key...');
-                                    jsonObj = newencryptor.encrypt(jsonObj);
-                                    console.log('Encrypted file!');
-                                    console.log('Output:' + jsonObj);
-                                    console.log('Writing data to file!');
-                                    try {
-                                      json = JSON.stringify(jsonObj, null, 2);
-                                      fs.writeFile(RootFolder + `/data/member/${file}`, json, 'utf8', function (err) {
-                                        console.log(`Converted ${file} to new key.`);
-                                        console.log('\x1b[32m%s\x1b[0m', `--------------------------------------------------------------------`);
-                                        if (files.length === index + 1) {
-                                          console.log('\x1b[34m%s\x1b[0m', 'Finnished converting all files succesfully!');
-                                          console.log('Updating token...');
-                                          store.add('config', 'token', newkey, function (err, object) {
-                                            if (err) throw err;
-                                            console.log('\x1b[34m%s\x1b[0m', 'Updated token succesfully!');
-                                            console.log('Updating client ID...');
-                                            store.add('config', 'clientId', ClientID, function (err, object) {
-                                              if (err) throw err;
-                                              rl.question('Insert chart API token from chart-img.com (optional): ', function (ChartKey) {
-                                                store.add('config', 'ChartKey', ChartKey, function(err, object){
-                                                  if(err) throw err;
-                                                  console.log('\033[1A' + 'Insert chart API token from chart-img.com (optional): ******************************************************************');
-                                                  console.log('\x1b[34m%s\x1b[0m', 'Updated chart API key succesfully!');
-                                                  rl.close();
+              rl.question('Insert chart API token from chart-img.com (optional): ', function (ChartKey) {
+                store.add('config', 'ChartKey', ChartKey, function (err, object) {
+                  if (err) throw err;
+                  console.log('\033[1A' + 'Insert chart API token from chart-img.com (optional): ******************************************************************');
+                  console.log('\x1b[34m%s\x1b[0m', 'Updated chart API key succesfully!');
+                  rl.close();
+                  fs.readdir(RootFolder + '/data/member/', function (err, files) {
+                    if (err) {
+                      console.error("Could not list the directory.", err);
+                      process.exit(1);
+                    }
+                    if (files.length) {
+                      var encryptor = require('simple-encryptor')(Token);
+                      var newencryptor = require('simple-encryptor')(newkey);
+                      files.forEach(function (file, index) {
+                        fs.readFile(RootFolder + `/data/member/${file}`, 'utf8', function (err, code) {
+                          if (err) return cb("error loading file" + err);
+                          try {
+                            console.log('\x1b[32m%s\x1b[0m', `-------------------${file}-------------------`);
+                            var jsonObj = JSON.parse(code);
+                            console.log('Decrypting file...');
+                            jsonObj = encryptor.decrypt(jsonObj);
+                            console.log('Output:' + jsonObj);
+                            console.log('Encrypting file with new key...');
+                            jsonObj = newencryptor.encrypt(jsonObj);
+                            console.log('Encrypted file...');
+                            console.log('Output:' + jsonObj);
+                            jsonObj = newencryptor.decrypt(jsonObj);
+                            console.log('Decrypted file with new key...');
+                            console.log('Output:' + jsonObj);
+                            const IsDataGood = IsGoodString(JSON.stringify(jsonObj));
+                            console.log('Is Data Good: ' + IsDataGood);
+                            if (IsDataGood) {
+                              console.log('\x1b[32m%s\x1b[0m', `-------------------------------------------------------------`);
+                              if (files.length === index + 1) {
+                                if (AreFilesGood) {
+                                  console.log('Finnished converting all files succesfully!');
+                                  console.log('Wrting all files with converted data...');
+                                  files.forEach(function (file, index) {
+                                    fs.readFile(RootFolder + `/data/member/${file}`, 'utf8', function (err, code) {
+                                      if (err) return cb("error loading file" + err);
+                                      try {
+                                        console.log('\x1b[32m%s\x1b[0m', `-------------------Writing ${file}-------------------`);
+                                        var jsonObj = JSON.parse(code);
+                                        console.log('Decrypting file...');
+                                        jsonObj = encryptor.decrypt(jsonObj);
+                                        console.log('Output:' + jsonObj);
+                                        console.log('Encrypting file with new key...');
+                                        jsonObj = newencryptor.encrypt(jsonObj);
+                                        console.log('Encrypted file!');
+                                        console.log('Output:' + jsonObj);
+                                        console.log('Writing data to file!');
+                                        try {
+                                          json = JSON.stringify(jsonObj, null, 2);
+                                          fs.writeFile(RootFolder + `/data/member/${file}`, json, 'utf8', function (err) {
+                                            console.log(`Converted ${file} to new key.`);
+                                            console.log('\x1b[32m%s\x1b[0m', `--------------------------------------------------------------------`);
+                                            if (files.length === index + 1) {
+                                              console.log('\x1b[34m%s\x1b[0m', 'Finnished converting all files succesfully!');
+                                              console.log('Updating token...');
+                                              store.add('config', 'token', newkey, function (err, object) {
+                                                if (err) throw err;
+                                                console.log('\x1b[34m%s\x1b[0m', 'Updated token succesfully!');
+                                                console.log('Updating client ID...');
+                                                store.add('config', 'clientId', ClientID, function (err, object) {
+                                                  if (err) throw err;
                                                   cb();
                                                 });
                                               });
-                                            });
+                                            }
+                                            if (err) return cb(err);
                                           });
                                         }
-                                        if (err) return cb(err);
-                                      });
-                                    }
-                                    catch (e) {
-                                      console.log(e)
-                                    }
-                                  }
-                                  catch (e) {
-                                    console.log("Error parsing " + f + ": " + e);
-                                  }
-                                });
-                              });
+                                        catch (e) {
+                                          console.log(e)
+                                        }
+                                      }
+                                      catch (e) {
+                                        console.log("Error parsing " + f + ": " + e);
+                                      }
+                                    });
+                                  });
+                                } else {
+                                  console.log('\x1b[31m%s\x1b[0m', 'Cannot write new files since atleast one file corruted!');
+                                }
+                              } else {
+                                console.log('Finnished data convertation for ' + file);
+                              }
                             } else {
-                              console.log('\x1b[31m%s\x1b[0m', 'Cannot write new files since atleast one file corruted!');
+                              AreFilesGood = false;
+                              console.log(`Error decrypting file ${file} with new key!`);
+                              console.log(`${file} is most likely corrupted!`);
+                              console.log('\x1b[31m%s\x1b[0m', `-------------------------------------------------------------`);
                             }
-                          } else {
-                            console.log('Finnished data convertation for ' + file);
                           }
-                        } else {
-                          AreFilesGood = false;
-                          console.log(`Error decrypting file ${file} with new key!`);
-                          console.log(`${file} is most likely corrupted!`);
-                          console.log('\x1b[31m%s\x1b[0m', `-------------------------------------------------------------`);
-                        }
-                      }
-                      catch (e) {
-                        console.log("Error parsing " + f + ": " + e);
-                      }
-                    });
-                  });
-                } else {
-                  console.log('Data directory is empty!');
-                  console.log('Updating token...');
-                  store.add('config', 'token', newkey, function (err, object) {
-                    if (err) throw err;
-                    console.log('\x1b[34m%s\x1b[0m', 'Updated token succesfully!');
-                    console.log('Updating client ID...');
-                    store.add('config', 'clientId', ClientID, function (err, object) {
-                      if (err) throw err;
-                      console.log('\x1b[34m%s\x1b[0m', 'Updated client ID succesfully!');
-                      var DeployWizard = require(RootFolder + '/scripts/deploy-commands.js')('.');
-                      DeployWizard.deploy(function () {
-                        rl.close();
-                        cb();
+                          catch (e) {
+                            console.log("Error parsing " + f + ": " + e);
+                          }
+                        });
                       });
-                    });
+                    } else {
+                      console.log('Data directory is empty!');
+                      console.log('Updating token...');
+                      store.add('config', 'token', newkey, function (err, object) {
+                        if (err) throw err;
+                        console.log('\x1b[34m%s\x1b[0m', 'Updated token succesfully!');
+                        console.log('Updating client ID...');
+                        store.add('config', 'clientId', ClientID, function (err, object) {
+                          if (err) throw err;
+                          console.log('\x1b[34m%s\x1b[0m', 'Updated client ID succesfully!');
+                          var DeployWizard = require(RootFolder + '/scripts/deploy-commands.js')('.');
+                          DeployWizard.deploy(function () {
+                            rl.close();
+                            cb();
+                          });
+                        });
+                      });
+                    }
                   });
-                }
+                });
               });
             } else {
               rl.close();
