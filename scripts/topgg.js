5c61f2e667dbd801b99dfc31e8729e0855c45283
Jaakko <54206348+JAAKKQ@users.noreply.github.com>
2022-04-15 16:31:15 +0300

    Update topgg.js

--- scripts/topgg.js
+++ scripts/topgg.js
@@ -2,24 +2,25 @@ const express = require('express')
 const Topgg = require('@top-gg/sdk')
 const { dirname } = require('path');
 const RootFolder = dirname(require.main.filename);
-const { TopGGkey } = require(RootFolder + '/config.json');
-
-const webhook = new Topgg.Webhook(TopGGkey) // add your Top.gg webhook authorization (not bot token)
-
-const app = express() // Your express app
-
-app.post('/dblwebhook', webhook.listener(vote => {
-  // vote is your vote object
-  console.log(vote.user) // 221221226561929217
-})) // attach the middleware
 
 module.exports = function (dir) {
   return {
     dir: dir,
 
     init: function () {
-      console.log('Starting Top.gg server...')
+      console.log('Starting Top.gg server...');
+      const { TopGGkey } = require(RootFolder + '/config.json');
+      const webhook = new Topgg.Webhook(TopGGkey) // add your Top.gg webhook authorization (not bot token)
+      const app = express() // Your express app
+      bindWeb();
       app.listen(3000) // your port
     }
   }
 };
+
+function bindWeb() {
+  app.post('/dblwebhook', webhook.listener(vote => {
+    // vote is your vote object
+    console.log(vote.user) // 221221226561929217
+  })) // attach the middleware
+}
