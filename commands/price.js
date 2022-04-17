f73e866352d798e0edb2d459a68239745e0e0386
Jaakko <54206348+JAAKKQ@users.noreply.github.com>
2022-04-17 16:30:04 +0300

    Ephemeral experiments

--- commands/price.js
+++ commands/price.js
@@ -5,11 +5,12 @@ const isEmptyObject = (obj) => Object.keys(obj).length === 0;
 const { dirname } = require('path');
 const RootFolder = dirname(require.main.filename);
 
-function InvalidCoin(interaction, coin){
+async function InvalidCoin(interaction, coin){
+	await interaction.deferReply({ ephemeral: true });
 	const exampleEmbed = new MessageEmbed()
 		.setColor('#E74C3C')
 		.setTitle(`${coin} is not valid!`);
-	interaction.editReply({ embeds: [exampleEmbed] });
+	interaction.editReply({ embeds: [exampleEmbed], ephemeral: true });
 }
 
 module.exports = {
@@ -27,11 +28,11 @@ module.exports = {
 		const ccupper = (cc.toUpperCase());
 		const response = await fetch(`https://api.coingecko.com/api/v3/coins/${cc}`);
 		const data = await response.json();
-		await interaction.deferReply();
 		if (isEmptyObject(data)) {
 			InvalidCoin(interaction, ccupper);
 		}
 		if (!isEmptyObject(data) && !JSON.stringify(data).includes('error')) {
+			await interaction.deferReply();
 			const usdprice = data.market_data.current_price.usd;
 			const pricechange = data.market_data.price_change_24h;
 			const exampleEmbed = new MessageEmbed()
