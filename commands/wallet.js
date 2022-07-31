6bb62db3bbf90591c690a56b578d07b0fb8d3d8d
Jaakko <mail@r3ne.net>
2022-07-31 19:47:46 +0300

    Wallet overview and updated update alert function

--- commands/wallet.js
+++ commands/wallet.js
@@ -21,11 +21,60 @@ module.exports = {
   async execute(interaction) {
     if (!interaction.isCommand()) return;
     await interaction.deferReply();
-    const cc = interaction.options.getString('coin')
-    console.log(cc);
+    const Rawcc = interaction.options.getString('coin')
     const target = interaction.options.getUser('user') ?? interaction.user;
-    if (!cc === null) {
-      const cc = cc.toLowerCase()
+    if (Rawcc === null) {
+      if (fs.existsSync(RootFolder + '/data/member/' + target.id + '.json')) {
+        fs.readFile(RootFolder + '/data/member/' + target.id + '.json', 'utf8', function (err, code) {
+          if (err) return cb("error loading file" + err);
+          try {
+            var jsonObj = JSON.parse(code);
+            jsonObj = encryptor.decrypt(jsonObj);
+            const dataObj = Object.keys(jsonObj);
+            var coins = [];
+            dataObj.forEach(function (data, index) {
+              if (data.includes('COIN-')) {
+                store.load(target.id, data, function (err, object) {
+                  if (err) throw err;
+                  coins.push({ name: `${data.replace("COIN-", "")}`, value: `${object}` },);
+                  if (index === Object.keys(dataObj).length - 1) {
+                    if (coins.length > 0) {
+                      const exampleEmbed = new MessageEmbed()
+                        .setColor('#F1C40F')
+                        .setTitle(`Your wallet overview`)
+                        .addFields(
+                          coins
+                        )
+                        .setTimestamp();
+                      interaction.editReply({ embeds: [exampleEmbed] });
+                    }
+                  }
+                });
+              }
+              if (index === Object.keys(dataObj).length - 1) {
+                if (coins.length === 0) {
+                  const exampleEmbed = new MessageEmbed()
+                    .setColor('#F1C40F')
+                    .setTitle(`Your wallet is empty`)
+                    .setTimestamp();
+                  interaction.editReply({ embeds: [exampleEmbed] });
+                }
+              }
+            });
+          }
+          catch (e) {
+            console.log("Error parsing " + f + ": " + e);
+          }
+        });
+      } else {
+        const exampleEmbed = new MessageEmbed()
+          .setColor('#F1C40F')
+          .setTitle(`Your wallet is empty`)
+          .setTimestamp();
+        interaction.editReply({ embeds: [exampleEmbed] });
+      }
+    } else {
+      cc = Rawcc.toLowerCase();
       const ccupper = interaction.options.getString('coin').toUpperCase()
       const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${cc}&vs_currencies=usd`);
       const data = await response.json();
@@ -48,45 +97,6 @@ module.exports = {
           .setColor('#E74C3C')
           .setTitle(`${ccupper} is not supported!`);
         interaction.editReply({ embeds: [exampleEmbed] });
-      }
-    } else {
-      if (fs.existsSync(RootFolder + '/data/member/' + target.id + '.json')) {
-        fs.readFile(RootFolder + '/data/member/' + target.id + '.json', 'utf8', function (err, code) {
-          if (err) return cb("error loading file" + err);
-          try {
-            var jsonObj = JSON.parse(code);
-            jsonObj = encryptor.decrypt(jsonObj);
-            const dataObj = Object.keys(jsonObj);
-            var coins = [];
-            dataObj.forEach(function (data, index) {
-              if (!data.includes('COIN-')) {
-                coins.push(data);
-                if(dataObj.length >= index){
-                  const exampleEmbed = new MessageEmbed()
-                  .setColor('#F1C40F')
-                  .setTitle(`Your wallet overview`)
-                  .addFields(
-                    { name: 'Bitcoin', value: `0 BTC (0 USD)` },
-                  )
-                  .setTimestamp();
-                interaction.editReply({ embeds: [exampleEmbed] });
-                }
-              }
-            });
-          }
-          catch (e) {
-            console.log("Error parsing " + f + ": " + e);
-          }
-        });
-      } else {
-        const exampleEmbed = new MessageEmbed()
-        .setColor('#F1C40F')
-        .setTitle(`Your wallet overview`)
-        .addFields(
-          { name: 'Bitcoin', value: `0 BTC (0 USD)` },
-        )
-        .setTimestamp();
-      interaction.editReply({ embeds: [exampleEmbed] });
       }
     }
   },
